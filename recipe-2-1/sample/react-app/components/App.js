44d6889ae862d00dfc249c222ad850496e4d85d3
hironomiu <hironomiu@gmail.com>
2021-08-02 13:33:59 +0900

    commited

--- recipe-2-1/sample/react-app/components/App.js
+++ recipe-2-1/sample/react-app/components/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 
+const API_URL = 'http://localhost:5000/api/users'
+
 const userEntry = async (name, setUsers, setName) => {
   const bodyData = JSON.stringify({ name: name })
 
-  const res = await fetch('http://localhost:5000/api/users', {
+  const res = await fetch(API_URL, {
     method: 'POST',
     mode: 'cors',
     cache: 'no-cache',
@@ -19,7 +21,7 @@ const userEntry = async (name, setUsers, setName) => {
 }
 
 const getUsers = async (setUsers) => {
-  const res = await fetch('http://localhost:5000/api/users')
+  const res = await fetch(API_URL)
   const json = await res.json()
   setUsers([...json])
 }
@@ -27,10 +29,19 @@ const getUsers = async (setUsers) => {
 const App = () => {
   const [name, setName] = useState('bob')
   const [users, setUsers] = useState([])
+  const [buttonIs, setButtonIs] = useState(true)
 
   useEffect(() => {
     getUsers(setUsers)
   }, [])
+
+  useEffect(() => {
+    if (name.length !== 0) {
+      setButtonIs(false)
+    } else {
+      setButtonIs(true)
+    }
+  }, [name])
 
   return (
     <div>
@@ -41,13 +52,20 @@ const App = () => {
       ))}
       <input
         type="text"
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => {
+          setName(e.target.value)
+          if (name.length !== 0) {
+            setButtonIs(false)
+          }
+        }}
         value={name}
       />
       <button
         onClick={() => {
           userEntry(name, setUsers, setName)
+          setButtonIs(true)
         }}
+        disabled={buttonIs}
       >
         push
       </button>
