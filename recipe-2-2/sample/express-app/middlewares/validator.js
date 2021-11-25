0e13c783a0fed5a204fb7b1c84d14c8d91758e61
hironomiu <hironomiu@gmail.com>
2021-11-25 11:15:02 +0900

    commited

--- recipe-2-2/sample/express-app/middlewares/validator.js
+++ recipe-2-2/sample/express-app/middlewares/validator.js
@@ -1,4 +1,4 @@
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator')
 
 const checkEmailIsEmpty = check('email')
   .not()
